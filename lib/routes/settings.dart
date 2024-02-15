78545db1b5efa88c2838e34cc32f49211d48e909
Emmanuel E <49331376+TalentedSunday@users.noreply.github.com>
2024-02-15 18:14:03 +0000

    Update settings.dart
    74

--- lib/routes/settings.dart
+++ lib/routes/settings.dart
@@ -71,7 +71,8 @@ class _SettingsPageState extends State<SettingsPage> {
                       )
 
                     ],
-                  )),
+                  )
+              ),
             ],
           ),
         ),
