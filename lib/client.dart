21a109327230784fb38df6a986cd56430747daf5
Emmanuel E <49331376+TalentedSunday@users.noreply.github.com>
2024-02-15 16:15:09 +0000

    Update client.dart
    54 line

--- lib/client.dart
+++ lib/client.dart
@@ -50,8 +50,7 @@ class Client {
   }
 
   static Future<Map<dynamic, dynamic>> setAddressNote(addressId, note) async {
-    return makeRequest('put',
-                       
+    return makeRequest('put',                
       path: '/addresses/$addressId/notes',
       unauthorized: (() => Authentication.logout()),
       body: {'note': note},
